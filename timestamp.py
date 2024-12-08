8f10c615a5281b3da2e4e9f1689788a71ea5512f
不留 <weinibuliu@outlook.com>
2024-12-09 02:55:23 +0800

    fix

--- timestamp.py
+++ timestamp.py
@@ -1,6 +1,6 @@
 import time
 import subprocess
 
-timestamp = time.time()
-subprocess.check_call(f'echo TIMESTAMP={timestamp} >> "$GITHUB_ENV"', shell=True)
+timestamp = int(time.time())
+subprocess.check_call(f'echo "TIMESTAMP={timestamp}" >> "$GITHUB_ENV"', shell=True)
 print(timestamp)
